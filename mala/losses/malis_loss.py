Fix potential div-by-0 during normalization in malis loss

mala/losses/malis_loss.py
@@ -14,22 +14,12 @@ class MalisWeights(object):
 
         assert affs.shape[0] == len(self.neighborhood)
 
-        weights_neg = self.malis_pass(affs, gt_seg, pos=0)
-        weights_pos = self.malis_pass(affs, gt_seg, pos=1)
+        weights_neg = self.malis_pass(affs, gt_affs, gt_seg, pos=0)
+        weights_pos = self.malis_pass(affs, gt_affs, gt_seg, pos=1)
 
-        # positive samples don't contribute in the negative pass
-        weights_neg[gt_affs==1] = 0
-        weights_neg = weights_neg/np.sum(weights_neg)
+        return weights_neg + weights_pos
 
-        # negative samples don't contribute in the positive pass
-        weights_pos[gt_affs==0] = 0
-        weights_pos = weights_pos/np.sum(weights_pos)
-
-        weights = weights_neg + weights_pos
-
-        return weights
-
-    def malis_pass(self, affs, gt_seg, pos):
+    def malis_pass(self, affs, gt_affs, gt_seg, pos):
 
         weights = malis.malis_loss_weights(
             gt_seg.astype(np.uint64).flatten(),
@@ -41,7 +31,16 @@ class MalisWeights(object):
         weights = weights.reshape((-1,) + tuple(self.output_shape))
         assert weights.shape[0] == len(self.neighborhood)
 
-        return weights.astype(np.float32)
+        # '1-pos' samples don't contribute in the 'pos' pass
+        weights[gt_affs == (1 - pos)] = 0
+
+        # normalize
+        weights = weights.astype(np.float32)
+        num_pairs = np.sum(weights)
+        if num_pairs > 0:
+            weights = weights/num_pairs
+
+        return weights
 
 def malis_weights_op(affs, gt_affs, gt_seg, neighborhood, name=None):
     '''Returns a tensorflow op to compute the weights of the MALIS loss. This